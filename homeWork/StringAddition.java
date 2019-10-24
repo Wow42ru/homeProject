917a888050704697407589042f06588153ede4dc
Wow42ru <wow42ru@gmail.ru>
2019-10-24 17:34:22 +0300

    1

---
+++ homeWork/StringAddition.java
@@ -0,0 +1,37 @@
+package homeProject.homeWork;
+//TODO: задачи на строки
+
+
+/*Найдите самое длинное слово в предложении, при условии, что в предложении все слова разной длины*/
+class Include {
+    /*Имеются две строки. Найти количество вхождений одной строки в другую.*/
+
+    // indexOf("символ") - для поиска первого вхождения указанного символа
+    // либо подстроки (вернет индекс)
+    // lastIndexOf - для поиска последнего вхождения указанного символа
+    // либо подстроки (вернет индекс)
+    // -1 -если символ или подстрока не найдены
+
+}
+
+public class StringAddition {
+    /*Даны 2 слова, состоящие из четного числа букв.
+    Получить слово состоящее из первой половины первого слова и второй половины второго слова.*/
+    String str1 = "Первое";
+    String str2 = "второе";
+    String newStr1 = str1.substring(0, str1.length() / 2);
+    String newStr2 = str2.substring(str2.length() / 2, str2.length());
+    String newStr = newStr1 + newStr2;
+
+    void print() {
+        System.out.println(newStr);
+    }
+
+}
+
+class test {
+    public static void main(String[] args) {
+        StringAddition stringAddition = new StringAddition();
+        stringAddition.print();
+    }
+}
